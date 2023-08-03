9091daeea9bb9aeb6836812086d715c21fbeac9f
Lior Lahav <LahavLior@gmail.com>
2023-08-03 11:15:43 +0300

    Fixed: Example

--- Example/example.cpp
+++ Example/example.cpp
@@ -1,22 +1,34 @@
+#include <iostream>
 #include <ImageLoader.h>
-#include <LLUtils/FileHelper.h>
-#include <ImageCodec.h>
+#include  <LLUtils/StopWatch.h>
 
 int main()
 {
 	// In this example, cat.jpg is being loaded and converted into cat.png
 	using namespace IMCodec;
-	ImageCodec codec;
 	ImageSharedPtr out_image;
-	IMCodec::ImageLoader loader(&codec, nullptr);
-	auto inputFileBuffer = LLUtils::File::ReadAllBytes(L"cat.jpg");
-	if (codec.Decode(inputFileBuffer.data(), inputFileBuffer.size(), {}, ImageLoadFlags::None, {}, {}, out_image)
+	IMCodec::ImageLoader loader;
+	int resultCode{};
+	if (loader.Decode(L"cat.jpg",ImageLoadFlags::None, {},PluginTraverseMode::AnyPlugin,out_image)
 		== ImageResult::Success)
 	{
+		std::cout << "\ncat.jpg has been decoded in: " << std::setprecision(2) << std::fixed << out_image->GetProcessData().processTime << " ms";
+
 		LLUtils::Buffer encodedBuffer;
-		auto pluginID = loader.GetFirstPlugin(L"png");
+		LLUtils::StopWatch encodeTime(true);
 		if (loader.Encode(out_image, L"./output/cat.png") == ImageResult::Success)
-			return 0;
+		{
+			
+			std::cout << "\ncat.png has been encoded in: " << std::setprecision(2) << std::fixed<< encodeTime.GetElapsedTimeReal(LLUtils::StopWatch::Milliseconds) << " ms";
+		}
 	}
-	return 1;
+	else
+	{
+		resultCode = 1;
+	}
+
+	std::cout << "\nPress any key to continue...";
+	getchar();
+
+	return resultCode;
 }
